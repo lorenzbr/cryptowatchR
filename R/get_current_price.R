30d9cb599a1ab338c6406eee00746638c6fdc1fe
lorenzbr <lorenz.brachtendorf@gmx.de>
2021-06-06 17:24:01 +0200

    new function get current price and get ohlc prices

---
+++ R/get_current_price.R
@@ -0,0 +1,60 @@
+#' Get current price of cryptocurrencies
+#'
+#' Get current price of cryptocurrencies using the REST API of cryptowat.ch. The route is \emph{price} or \emph{prices} and returns the current price of a given pair or current prices of all pairs.
+#'
+#' @usage get_current_price(pair, exchange = "kraken", api_key = NULL, allowance = FALSE)
+#' @param pair A string containing a pair symbol, e.g. \emph{btcusd} (required argument). Run \code{get_pairs()} to find other available pairs.
+#' @param exchange A string containing the exchange. Default is \emph{kraken}. Run \code{get_exchanges()} to find other available exchanges.
+#' @param api_key A string containing the API key. See \url{https://docs.cryptowat.ch/rest-api/rate-limit} to learn how to create an account and how to generate an API key.
+#' @param allowance A logical (default is \code{FALSE}). If \code{TRUE} the function returns a list which includes allowance information, i.e. cost of the request, remaining credits and your account name.
+#'
+#' @return Current price of a given pair of currencies. If allowance is \code{TRUE}, \code{get_ohlc()} returns a list.
+#'
+#' @references See \url{https://docs.cryptowat.ch/rest-api} for further information.
+#' @seealso \code{\link{get_markets}}, \code{\link{get_ohlc}}, \code{\link{get_exchanges}}, \code{\link{get_pairs}}
+#' @examples
+#' \dontrun{
+#' # Daily prices of Bitcoin in USD
+#' current.price <- get_current_price("btcusd")
+#' current.prices <- get_current_price()
+#' }
+#' @export
+get_current_price <- function(pair = NULL, exchange = "kraken", api_key = NULL, allowance = FALSE) {
+
+  if ( !is.null(pair) ) route <- "price" else if ( is.null(pair) ) route <- "prices"
+
+  prices <- get_markets(route = route, pair = pair, exchange = exchange, api_key = api_key, allowance = allowance)
+
+  if (allowance) {
+
+    allowance.list <- prices[[2]]
+    prices <- prices[[1]]
+
+  }
+
+
+  if ( !is.null(pair) ) {
+
+    if (allowance) { price.out <- prices } else if (allowance == FALSE) { price.out <- prices$price }
+
+  } else if ( is.null(pair) ) {
+
+    price.out <- data.frame(name = names(prices), price = unlist(prices))
+    rownames(price.out) <- 1:nrow(price.out)
+
+  }
+
+
+  if (allowance) {
+
+    output <- list(result = price.out, allowance = allowance.list)
+
+  } else if (allowance == FALSE) {
+
+    output <- price.out
+
+  }
+
+  return(output)
+
+}
--- R/get_ohlc.R
+++ R/get_ohlc.R
@@ -2,7 +2,7 @@
 #'
 #' Get data.frame with prices of cryptocurrencies using the REST API of cryptowat.ch. The route is \emph{ohlc} and returns OHLC candlestick prices. The default is daily prices but can be changed with \code{params} and \code{periods}.
 #'
-#' @usage markets(pair, params = NULL, exchange = "kraken", datetime = TRUE,
+#' @usage get_ohlc(pair, params = NULL, exchange = "kraken", datetime = TRUE,
 #'         api_key = NULL, allowance = FALSE)
 #' @param pair A string containing a pair symbol, e.g. \emph{btcusd} (required argument). Run \code{get_pairs()} to find other available pairs.
 #' @param params A list containing \code{before}, \code{after} and \code{periods} which is measured in seconds (optional). See \url{https://docs.cryptowat.ch/rest-api/markets/ohlc} for further information.
@@ -11,24 +11,24 @@
 #' @param api_key A string containing the API key. See \url{https://docs.cryptowat.ch/rest-api/rate-limit} to learn how to create an account and how to generate an API key.
 #' @param allowance A logical (default is \code{FALSE}). If \code{TRUE} the function returns a list which includes allowance information, i.e. cost of the request, remaining credits and your account name.
 #'
-#' @return A data.frame containing prices of a given pair of currencies. If allowance is \code{TRUE}, \code{markets()} returns a list.
+#' @return A data.frame containing prices of a given pair of currencies. If allowance is \code{TRUE}, \code{get_ohlc()} returns a list.
 #'
 #' @references See \url{https://docs.cryptowat.ch/rest-api} for further information.
 #' @seealso \code{\link{get_markets}}, \code{\link{get_assets}}, \code{\link{get_exchanges}}, \code{\link{get_pairs}}
 #' @examples
 #' \dontrun{
 #' # Daily prices of Bitcoin in USD
-#' df.btcusd <- markets("btcusd")
+#' df.btcusd <- get_ohlc("btcusd")
 #' # Hourly prices of Bitcoin in USD for a specific time period
-#' df.btcusd.hourly <- markets("btcusd", list(periods = 3600, before = 1609851600,
+#' df.btcusd.hourly <- get_ohlc("btcusd", list(periods = 3600, before = 1609851600,
 #'                                           after = 1609506000), datetime = FALSE)
 #' # Daily prices of Bitcoin in Euro for a specific time period
-#' df.btceur.daily <- markets("btceur", list(periods = 86400, before = "2021-05-12",
+#' df.btceur.daily <- get_ohlc("btceur", list(periods = 86400, before = "2021-05-12",
 #'                                           after = "2021-01-01"), datetime = TRUE)
 #' }
 #'
 #' @export
-markets <- function(pair, params = NULL, exchange = "kraken", datetime = TRUE, api_key = NULL, allowance = FALSE) {
+get_ohlc <- function(pair, params = NULL, exchange = "kraken", datetime = TRUE, api_key = NULL, allowance = FALSE) {
 
   route <- "ohlc"
 
